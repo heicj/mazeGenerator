b8839a6bdf417d26575466583bac18f1755f3122
Charlie <heinercj@gmail.com>
2020-08-10 23:58:07 -0700

    generating maze until run out of unvisited neighbors

--- Cell.js
+++ Cell.js
@@ -1,12 +1,65 @@
-export class Cell{
-  constructor(x, y, width, height){
-    this.x = x * width;
-    this.y = y * width;
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+export class Cell{ //x is row y is col
+  constructor(row, col, width, height, rows, columns){
+    this.row = row;
+    this.col = col;
+    this.x = col * width;
+    this.y = row * width;
     this.width = width;
     this.height = height;
+
+    this.totalRows = rows;
+    this.totalColumns = columns;
     this.wallColor = 'black';
     this.walls = [true, true, true, true]; //wall order is [Top, Right, Bottom, Left]
     this.visited = false;
+  }
+
+  index(row, col){
+    if(row < 0 || col < 0 || row > this.rows - 1 || col > this.columns - 1){
+      return -1;
+    } else {
+      return col + row * this.totalColumns; //figure out how to not hard code 10, use columns const from index.js
+    }
+  }
+
+  getNeighbor(grid){
+    let neighbors = [];
+
+    let top = grid[this.index((this.row - 1), this.col)];
+    let right = grid[this.index(this.row, (this.col + 1))];
+    let bottom = grid[this.index((this.row + 1), this.col)];
+    let left = grid[this.index(this.row, (this.col - 1))]
+
+    if(top && !top.visited){
+      neighbors.push(top)
+    }
+
+    if(right && !right.visited){
+      neighbors.push(right)
+    }
+
+    if(bottom && !bottom.visited){
+      neighbors.push(bottom)
+    }
+
+    if(left && !left.visited){
+      neighbors.push(left)
+    }
+
+    if(neighbors.length > 0 ){
+      const i = getRandomIntInclusive(0, neighbors.length -1)
+      return neighbors[i];
+    }else{
+      return undefined;
+    }
+
   }
 
   drawTopWall(ctx){
@@ -42,12 +95,14 @@ export class Cell{
   }
 
   draw(ctx){
-   for(let i = 0; i < this.walls.length; i++){
      if(this.walls[0]) this.drawTopWall(ctx);
      if(this.walls[1]) this.drawRightWall(ctx);
      if(this.walls[2]) this.drawBottomWall(ctx);
      if(this.walls[3]) this.drawLeftWall(ctx);
-   }
+
+     if(this.visited){
+       ctx.fillRect(this.x, this.y, this.width, this.height)
+     }
   }
 
 }
--- index.js
+++ index.js
@@ -5,25 +5,52 @@ const ctx = canvas.getContext('2d');
 
 
 
-const CANVAS_HEIGHT = 500;
-const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 400;
+const CANVAS_WIDTH = 400;
 canvas.height = CANVAS_HEIGHT;
 canvas.width = CANVAS_WIDTH;
 
-const columns = 10;
-const rows = 10;
-const cellWidth = CANVAS_WIDTH/columns;
-const cellHeight = CANVAS_HEIGHT/rows;
+// let columns;
+// let rows;
+const cellWidth = 40;
+const cellHeight = 40;
 
-ctx.fillStyle = 'purple';
+export const columns = CANVAS_WIDTH/cellWidth;
+export const rows = CANVAS_HEIGHT/cellHeight;
+
+ctx.fillStyle = 'lightblue';
 ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
 
 let cells = [];
 
+let current;
+
 for(let row = 0; row < rows; row++){
   for(let col = 0; col < columns; col++){
-    let cell = new Cell(row, col, cellWidth, cellHeight)
+    let cell = new Cell(row, col, cellWidth, cellHeight, rows, columns)
     cells.push(cell);
-    cell.draw(ctx);
   }
+
+  current = cells[0]
 }
+
+let prevTime = 0;
+function draw(timestamp){
+
+  for(let i = 0; i < cells.length; i++){
+    cells[i].draw(ctx)
+  }
+
+  current.visited = true;
+  let next = current.getNeighbor(cells)
+  if(next){
+    next.visited = true;
+    current = next;
+  }
+
+  window.requestAnimationFrame(draw)
+}
+
+draw();
+
+
